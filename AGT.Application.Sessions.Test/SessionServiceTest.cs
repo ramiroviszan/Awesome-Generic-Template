c838357f2f87b1a96e1810ceaede1036f5670df5
Ramiro V <ramiroviszan@gmail.com>
2018-08-09 20:29:51 -0300

    Test session login

--- AGT.Application.Sessions.Test/SessionServiceTest.cs
+++ AGT.Application.Sessions.Test/SessionServiceTest.cs
@@ -7,25 +7,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 
+
 namespace AGT.Application.Sessions.Test
 {
     [TestClass]
     public class SessionServiceTest
     {
         private ISessionService sessionService;
-        private Session user;
+        private User databaseUser;
         private Mock<IUnitOfWork> unitOfWork;
         private Mock<IHashGenerator> generator;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            unitOfWork = new Mock<IUnitOfWork>();
+            generator = new Mock<IHashGenerator>();
+            databaseUser = new User("User1", "user@mail.com", "July", "Musk", "pass", DateTime.Today);
+            sessionService = new SessionService(unitOfWork.Object, generator.Object);
+        }
 
         [TestMethod]
         public void LoginSucessTest()
         {
-            var databaseUser = new User("User1", "user@mail.com", "July", "Musk", "pass", DateTime.Today);
             var session = new Session() { Username = databaseUser.Username, Password = databaseUser.Password, Agent = "Chrome", Device = "Laptop" };
-
-            var loginUser = It.IsAny<User>();
+            var loginUser = new Mock<User>().Object;
             loginUser.Username = session.Username;
-            loginUser.Username = session.Password;
+            loginUser.Password = session.Password;
 
             unitOfWork.Setup(r => r.Users.Find(loginUser)).Returns(databaseUser);
             generator.Setup(g => g.GetHash(databaseUser.Password, databaseUser.PasswordSalt)).Returns(databaseUser.Password);
